7776cd9c0e6e70935743b1caff6c416b856d0258
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-27 11:41:44 -0300

    En proceso home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-
+    <main className="bg-svg-1 w-full h-fit pt-[112px]">
+      <h1 className="px-4 py-8 text-4xl text-pretty">
+        Bienvenido a <b className="title">Inter Q:</b> Tu Recurso Definitivo
+        para Preguntas de Entrevista en IT
+      </h1>
     </main>
   );
 }
